3fb7873f185ff5c8756c29b3788bcb5f1e793224
braveJalen <braveJalen@163.com>
2017-03-21 16:19:59 +0800

    Initial commit

---
+++ MaterialDesign/src/main/java/com/jalen/materialdesign/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.jalen.materialdesign.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jalen.materialdesign.R;
+
+/**
+ * @author Dragon
+ * @date 2017/3/21. 16:11
+ * @editor
+ * @date
+ * @describe
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
