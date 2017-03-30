eba36abf188f45b1a2ab47b664e0244198228808
braveJalen <braveJalen@163.com>
2017-03-30 15:50:02 +0800

    增加基本的属性

--- MaterialDesign/src/main/java/com/jalen/materialdesign/MainActivity.java
+++ MaterialDesign/src/main/java/com/jalen/materialdesign/MainActivity.java
@@ -1,10 +1,8 @@
-package com.jalen.materialdesign.activity;
+package com.jalen.materialdesign;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
-import com.jalen.materialdesign.R;
 
 /**
  * @author Dragon
