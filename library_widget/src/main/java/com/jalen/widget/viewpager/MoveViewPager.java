7cbfb5f3d81f5a657df14bc26037ee3e5dad6057
braveJalen <braveJalen@163.com>
2017-04-07 11:13:04 +0800

    添加library_widget

---
+++ library_widget/src/main/java/com/jalen/widget/viewpager/MoveViewPager.java
@@ -0,0 +1,17 @@
+package com.jalen.widget.viewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+
+/**
+ * @author Dragon
+ * @date 2017/4/7. 11:09
+ * @editor
+ * @date
+ * @describe
+ */
+public class MoveViewPager extends ViewPager {
+    public MoveViewPager(Context context) {
+        super(context);
+    }
+}
