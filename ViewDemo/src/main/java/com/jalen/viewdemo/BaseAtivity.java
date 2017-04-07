6fa8fa983ebcec7c7ed6f5cc1acbb6b8543a565d
braveJalen <braveJalen@163.com>
2017-04-07 10:57:48 +0800

    添加viewDemo

---
+++ ViewDemo/src/main/java/com/jalen/viewdemo/BaseAtivity.java
@@ -0,0 +1,13 @@
+package com.jalen.viewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Dragon
+ * @date 2017/4/7. 10:55
+ * @editor
+ * @date
+ * @describe
+ */
+public class BaseAtivity extends AppCompatActivity {
+}
---
+++ ViewDemo/src/main/java/com/jalen/viewdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jalen.viewdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @author Dragon
+ * @date 2017/4/7. 10:55
+ * @editor
+ * @date
+ * @describe
+ */
+public class MainActivity extends BaseAtivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
