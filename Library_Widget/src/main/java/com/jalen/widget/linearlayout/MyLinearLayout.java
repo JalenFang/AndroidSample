3a91bf743c92c5b24236e050d73bcfeed90e7613
braveJalen <braveJalen@163.com>
2017-04-17 15:01:29 +0800

    MyLinearLayout support margin

--- Library_Widget/src/main/java/com/jalen/widget/linearlayout/MyLinearLayout.java
+++ Library_Widget/src/main/java/com/jalen/widget/linearlayout/MyLinearLayout.java
@@ -3,6 +3,8 @@ package com.jalen.widget.linearlayout;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -30,15 +32,80 @@ public class MyLinearLayout extends ViewGroup {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        Log.i("dragon", "widthMode = " + widthMode);
+        Log.i("dragon", "widthSize = " + widthSize);
+
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        Log.i("dragon", "heightSize = " + heightSize);
+        Log.i("dragon", "heightMode = " + heightMode);
+
+        int height = 0;
+        int width = 0;
+
+        int childCount = getChildCount();
+
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+
+            MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+
+            int measuredHeight = child.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin;
+            int measuredWidth = child.getMeasuredWidth() + layoutParams.leftMargin + layoutParams.rightMargin;
+
+            height += measuredHeight;
+            width = Math.max(width, measuredWidth);
+        }
+
+        setMeasuredDimension(widthMode == MeasureSpec.EXACTLY ? widthSize : width, heightMode == MeasureSpec.EXACTLY ? heightSize : height);
+        int measuredHeight = getMeasuredHeight();
+        int measuredWidth = getMeasuredWidth();
+
+        Log.w("dragon", "measuredHeight = " + measuredHeight);
+        Log.w("dragon", "measuredWidth = " + measuredWidth);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        
+        int top = 0;
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+
+            MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+
+            /*int measuredHeight = child.getMeasuredHeight();
+            int measuredWidth = child.getMeasuredWidth();
+            child.layout(0, top, measuredWidth, top + measuredHeight);
+            top += measuredHeight;
+            */
+
+            child.layout(layoutParams.leftMargin, top + layoutParams.topMargin, child.getMeasuredWidth() + layoutParams.leftMargin, top + child.getMeasuredHeight() + layoutParams.topMargin);
+            top += child.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
     }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
+    }
+
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+
 }
