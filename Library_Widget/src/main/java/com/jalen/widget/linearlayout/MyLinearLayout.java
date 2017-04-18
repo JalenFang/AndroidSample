c27edbdae92cd934d9f7c6e4109cdb76052aca68
braveJalen <braveJalen@163.com>
2017-04-18 11:07:08 +0800

    MyLinearLayout 重写方法的注释

--- Library_Widget/src/main/java/com/jalen/widget/linearlayout/MyLinearLayout.java
+++ Library_Widget/src/main/java/com/jalen/widget/linearlayout/MyLinearLayout.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
  * @date 2017/4/17. 10:59
  * @editor
  * @date
- * @describe
+ * @describe 自定义ViewGroup
  */
 public class MyLinearLayout extends ViewGroup {
     public MyLinearLayout(Context context) {
@@ -27,9 +27,23 @@ public class MyLinearLayout extends ViewGroup {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * 测量MyLinearLayout的大小
+     *
+     * @param widthMeasureSpec  父View给MyLinearLayout宽度的建议值 包括测量的模式和大小
+     * @param heightMeasureSpec 父View给MyLinearLayout高度的建议值 包括测量的模式和大小
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+       /* MeasureSpec.getMode 获取测量模式
+        共有三种模式：
+        MeasureSpec.EXACTLY 精确的 父元素决定子元素的确切大小，子元素将被限定在给定的边界里而忽略它本身大小  match_parent
+        MeasureSpec.AT_MOST 子元素至多达到指定大小的值 wrap_content
+        MeasureSpec.UNSPECIFIED 父元素不对子元素施加任何束缚，子元素可以得到任意想要的大小*/
+
+
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         Log.i("dragon", "widthMode = " + widthMode);
@@ -66,6 +80,15 @@ public class MyLinearLayout extends ViewGroup {
         Log.w("dragon", "measuredWidth = " + measuredWidth);
     }
 
+    /**
+     * 设置子View的位置
+     *
+     * @param changed
+     * @param l
+     * @param t
+     * @param r
+     * @param b
+     */
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int top = 0;
