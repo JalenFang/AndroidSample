1c268049c12da0c31279889485c64adab7122b5f
braveJalen <braveJalen@163.com>
2017-04-21 15:06:36 +0800

    添加scrollTo

---
+++ ScrollDemo/src/main/java/com/jalen/scrolldemo/view/ScrollByView.java
@@ -0,0 +1,28 @@
+package com.jalen.scrolldemo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author Dragon
+ * @date 2017/4/21. 14:56
+ * @editor
+ * @date
+ * @describe
+ */
+public class ScrollByView extends View {
+    public ScrollByView(Context context) {
+        super(context);
+    }
+
+    public ScrollByView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollByView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
--- scrolldemo/src/main/java/com/jalen/scrolldemo/MainActivity.java
+++ scrolldemo/src/main/java/com/jalen/scrolldemo/MainActivity.java
@@ -1,12 +1,15 @@
 package com.jalen.scrolldemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.jalen.scrolldemo.activity.LayoutActivity;
 import com.jalen.scrolldemo.activity.LayoutParamsActivity;
 import com.jalen.scrolldemo.activity.OffSetActivity;
+import com.jalen.scrolldemo.activity.ScrollToActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 LayoutParamsActivity.startActivity(MainActivity.this);
+            }
+        });
+
+        findViewById(R.id.activity_main_btn_To_by).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.scroll_type)
+                        .setItems(R.array.scroll_style, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                if (which == 0) {//ScrollTo
+                                    ScrollToActivity.startActivity(MainActivity.this);
+                                } else if (which == 1) {//ScrollBy
+
+                                }
+                            }
+                        });
+
+                builder.show();
             }
         });
     }
