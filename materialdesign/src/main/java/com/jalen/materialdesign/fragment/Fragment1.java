fd159be15141573975177afb6eef634b9acff43c
braveJalen <braveJalen@163.com>
2017-05-02 16:41:25 +0800

    添加测试用例

---
+++ materialdesign/src/main/java/com/jalen/materialdesign/fragment/Fragment1.java
@@ -0,0 +1,26 @@
+package com.jalen.materialdesign.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * @author Dragon
+ * @date 2017/5/2. 16:02
+ * @editor
+ * @date
+ * @describe
+ */
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(getContext());
+        textView.setText("1");
+        return textView;
+    }
+}
